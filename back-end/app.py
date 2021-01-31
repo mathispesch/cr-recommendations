3946e36eeaa65135193ffb6c38a0575aafc613c1
Mathis <mathis.pesch@gmail.com>
2021-01-31 16:33:49 +0100

    [Flask] Base app

---
+++ back-end/app.py
@@ -0,0 +1,50 @@
+import os
+
+from flask import Flask, jsonify, request, session
+
+import tmdb
+
+app = Flask(__name__)
+app.secret_key = os.environ["SECRET"]
+
+
+@app.route("/movies")
+def get_n():
+    n = request.args.get("n", 10)
+    if not (5 < n < 20):
+        return jsonify({"success": False, "error": "n must be between 5 and 20."}), 400
+
+    movies = []
+    if "choices" not in session:
+        # The user has not choosen a movie yet
+        movies = tmdb.get_movies(n)
+    else:
+        # The user has already choosen at least one movie
+        movies = tmdb.get_movies(n)  # TODO: search and choose relevant movies
+
+    return jsonify({"success": True, "movies": movies})
+
+
+@app.route("/choice", methods=["POST"])
+def post_choice():
+    if not request.json or "choice" not in request.json:
+        return jsonify({"success": False, "error": "You must submit a movie choice."}), 400
+
+    if "choices" not in session:
+        session["choices"] = []
+
+    session["choices"] = session["choices"] + [request.json["choice"]]
+
+    return jsonify({"success": True})
+
+
+@app.route("/details/<_id>")
+def get_details(_id):
+    return jsonify({"success": True, "details": tmdb.get_movie(_id)})
+
+
+@app.route("/reset", methods=["POST"])
+def reset():
+    session.pop("choices")
+
+    return jsonify({"success": True})
---
+++ back-end/tmdb.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+import requests
+
+BEARER_TOKEN = os.environ["TMDB_TOKEN"]
+HEADERS = {
+    "Authorization": f"Bearer {BEARER_TOKEN}"
+}
+PARAMS = {
+    "language": "fr-FR",
+    "region": "FR"
+}
+
+BASE_API = "https://api.themoviedb.org/3/"
+
+
+def get_movies(n, filters=dict):
+    movies = []
+    for i in range(3 * n // 20 + 1):
+        r = requests.get(f"{BASE_API}/discover/movie", headers=HEADERS, params={**PARAMS, "page": i + 1})
+        if not r.ok:
+            print(r.json())
+            raise Exception("TMDB Error")
+
+        movies += r.json()["results"]
+
+    movies = random.choices(movies, k=10)
+    random.shuffle(movies)
+
+    return movies
+
+
+def get_movie(_id):
+    r = requests.get(
+        f"{BASE_API}/movie/{_id}", headers=HEADERS, params={**PARAMS, "append_to_response": "keywords, credits"}
+    )
+    if not r.ok:
+        raise Exception("TMDB Error")
+
+    return r.json()
